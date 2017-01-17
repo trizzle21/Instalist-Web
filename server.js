2d4f1d180fb2e9579c08f8102d9ce29f2f5cf982
trizzle21 <tdross21@gmail.com>
2017-01-16 16:36:34 -0800

    setting mongodb and express

--- server.js
+++ server.js
@@ -6,6 +6,53 @@ var server = app.listen(3000);
 var io = require('socket.io').listen(server);
 
 
+app.set('views', './views')
+app.set('view engine', 'pug')
+app.use('/static', express.static('static'))
+
+
 app.get('/', function (req, res) {
 	res.render('index')
 })
+
+
+/*
+"User Schema"
+{ 
+  firstName: "Tyler",
+  lastName: "Ross",
+  age: 24,
+  groups: [
+    "Ross Family",
+    "Stir",
+    "Mac > Windows"
+  ]
+}
+
+"Group Schema"
+{
+	groupName: "Ross Family",
+	size:3,
+	id: 14382045,
+	list_count: 0
+	lists: [
+		"Grocery 10/27",
+		"Grocery 11/4",
+		"Grocery 11/11"
+	]
+}
+
+List
+{
+	listName: "Grocery 10/27",
+	listItem: [
+		"cheese",
+		"bread",
+		"vodka"
+	]
+
+}
+
+
+
+*/
